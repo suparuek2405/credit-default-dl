561706975438599e066c8f2ee50767e42737e534
suparuek2405 <suparuek2405@gmail.com>
2026-06-02 05:24:05 +0000

    Add CreditDataset class to dataset.py

---
+++ src/dataset.py
@@ -0,0 +1,15 @@
+import torch
+from torch.utils.data import Dataset
+
+
+class CreditDataset(Dataset):
+    """PyTorch Dataset for credit card default data."""
+    def __init__(self, X, y):
+        self.X = torch.tensor(X, dtype=torch.float32)
+        self.y = torch.tensor(y, dtype=torch.float32)
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, idx):
+        return self.X[idx], self.y[idx]
